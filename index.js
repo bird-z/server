b61e907e94fd241242dd33db6cf7b5d718fe16c7
bird <z.x.peng@outlook.com>
2026-07-28 02:08:20 +0800

    finish 7.27

--- index.js
+++ index.js
@@ -1,132 +1,41 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mysql = require('mysql2/promise')
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'lcbird',
+  password: '1140',
+  database: 'newsdb'
+})
 
 app.use(cors())
 app.use(express.json())
 
-let news=[
-{"id":0,"title": "如何正确安慰兄弟", "url": "https://www.bilibili.com/video/av116911861926661/", "flow": 6419000},
-{"id":1,"title": "寻找卢本伟", "url": "https://www.bilibili.com/video/av116922565788910/", "flow": 4432000},
-{"id":2,"title": "路边摊吃盒饭 吃成摊主", "url": "https://www.bilibili.com/video/av116916760877064/", "flow": 4367000},
-{"id":3,"title": "《崩坏：星穹铁道》姬子•启行角色PV——「启明星于今夜坠落」", "url": "https://www.bilibili.com/video/av116915922011009/", "flow": 4083000},
-{"id":4,"title": "郝 哥 连 线 勇 哥", "url": "https://www.bilibili.com/video/av116921257170604/", "flow": 6255000},
-{"id":5,"title": "耗时9999小时！一口气看完熊出没末路大合集！", "url": "https://www.bilibili.com/video/av116915485808224/", "flow": 3748000},
-{"id":6,"title": "哭着剪完这条视频…", "url": "https://www.bilibili.com/video/av116913422272167/", "flow": 4528000},
-{"id":7,"title": "Bro有话说2", "url": "https://www.bilibili.com/video/av116917431964085/", "flow": 1810000},
-{"id":8,"title": "我这视频看完没啥感觉，但是暖暖的！", "url": "https://www.bilibili.com/video/av116917683620779/", "flow": 2179000},
-{"id":9,"title": "我宁愿在这个世界里，永远做一个你“尚未到来”的传说", "url": "https://www.bilibili.com/video/av116918237272151/", "flow": 3206000},
-{"id":10,"title": "搞强拆能惹多大祸？【奇葩小国53】", "url": "https://www.bilibili.com/video/av116917935347796/", "flow": 2076000},
-{"id":11,"title": "回 旋 彪", "url": "https://www.bilibili.com/video/av116916358290315/", "flow": 2798000},
-{"id":12,"title": "带小猎豹自律的一天", "url": "https://www.bilibili.com/video/av116916760872060/", "flow": 2534000},
-{"id":13,"title": "孩子总把网络烂梗挂嘴边怎么办？【AI全民制作人】", "url": "https://www.bilibili.com/video/av116901862711292/", "flow": 9942000},
-{"id":14,"title": "The Verity Cast Plays The REAL Verity Mod", "url": "https://www.bilibili.com/video/av116925166325236/", "flow": 986000},
-{"id":15,"title": "【幼稚园TV】建材王哥大闹天宫", "url": "https://www.bilibili.com/video/av116923169834068/", "flow": 2229000},
-{"id":16,"title": "我们好久没在一起玩了呢，桑多涅~", "url": "https://www.bilibili.com/video/av116911828572853/", "flow": 1403000},
-{"id":17,"title": "【SINOS DE NATAL】|“【对视の小曲】”", "url": "https://www.bilibili.com/video/av116900386316102/", "flow": 1576000},
-{"id":18,"title": "我把你们奇怪的想法都做到了我的世界里#14", "url": "https://www.bilibili.com/video/av116924361087995/", "flow": 1935000},
-{"id":19,"title": "\"自古英雄出少年.\"", "url": "https://www.bilibili.com/video/av116911056622772/", "flow": 3226000},
-{"id":20,"title": "《最失败の高情商》", "url": "https://www.bilibili.com/video/av116913858545491/", "flow": 4478000},
-{"id":21,"title": "打工人怨气最大的一集", "url": "https://www.bilibili.com/video/av116921257166588/", "flow": 2663000},
-{"id":22,"title": "神秘npc之兰州拉面！", "url": "https://www.bilibili.com/video/av116911828506810/", "flow": 5017000},
-{"id":23,"title": "这样的挑战下次请不要喊我了！#挑战# #野兽先生# #MrBeast# #vlog# #整活#", "url": "https://www.bilibili.com/video/av116917750793981/", "flow": 4354000},
-{"id":24,"title": "给村里修了条公路！", "url": "https://www.bilibili.com/video/av116906476444329/", "flow": 1155000},
-{"id":25,"title": "假如你在一个十万人宿舍查寝【AI全民制作人】", "url": "https://www.bilibili.com/video/av116911291500446/", "flow": 2603000},
-{"id":26,"title": "魔怔萌二和暴躁粗鲁女的相亲历险记", "url": "https://www.bilibili.com/video/av116923622819533/", "flow": 3153000},
-{"id":27,"title": "“乘凉不应该是一种特权”法国共产党将总部对外开放供民众纳凉", "url": "https://www.bilibili.com/video/av116917314525944/", "flow": 2918000},
-{"id":28,"title": "【复制铜镜全系列合集】ai软件生成，剧情虚构，非官方）正片+番外+解说+彩蛋", "url": "https://www.bilibili.com/video/av116909848662450/", "flow": 9423000},
-{"id":29,"title": "逆天小游戏2327", "url": "https://www.bilibili.com/video/av116917197411817/", "flow": 3113000},
-{"id":30,"title": "原摄玩家镜头下，你未曾见过的桑多涅......", "url": "https://www.bilibili.com/video/av116924612614009/", "flow": 951000},
-{"id":31,"title": "《兄弟有个妹妹》3", "url": "https://www.bilibili.com/video/av116923421428560/", "flow": 2948000},
-{"id":32,"title": "烧一桌中式家常菜，款待十年没见的法国老友！", "url": "https://www.bilibili.com/video/av116917549468229/", "flow": 1866000},
-{"id":33,"title": "这下真的能住了哈", "url": "https://www.bilibili.com/video/av116906090633993/", "flow": 4678000},
-{"id":34,"title": "几天没更新因为在做梦，这次的梦有点麻烦要处理事有点多所以在梦里呆了好几天才回来写歌", "url": "https://www.bilibili.com/video/av116910184208950/", "flow": 3732000},
-{"id":35,"title": "日服堵桥被全图围剿！鲁鲁正义の制裁", "url": "https://www.bilibili.com/video/av116917834618203/", "flow": 3332000},
-{"id":36,"title": "那还说啥了", "url": "https://www.bilibili.com/video/av116917599799117/", "flow": 2295000},
-{"id":37,"title": "举起小猫桑多涅", "url": "https://www.bilibili.com/video/av116900940091769/", "flow": 2463000},
-{"id":38,"title": "“ 网 咖 四 区 兄 弟 ”", "url": "https://www.bilibili.com/video/av116916710540951/", "flow": 1446000},
-{"id":39,"title": "完全看不懂中国网络梗的日本人，彻底崩溃了！", "url": "https://www.bilibili.com/video/av116916140183988/", "flow": 1553000},
-{"id":40,"title": "良子在美国良子面前也就是个萝莉！", "url": "https://www.bilibili.com/video/av116911996147810/", "flow": 2590000},
-{"id":41,"title": "归寂速通星穹列车胜利结算画面", "url": "https://www.bilibili.com/video/av116908422597920/", "flow": 2313000},
-{"id":42,"title": "我找到了三角洲里的零号大坝！？", "url": "https://www.bilibili.com/video/av116917968902957/", "flow": 1431000},
-{"id":43,"title": "胜负已分", "url": "https://www.bilibili.com/video/av116921458563939/", "flow": 1147000},
-{"id":44,"title": "《小时候爱看的视频 be like》", "url": "https://www.bilibili.com/video/av116911861925512/", "flow": 3916000},
-{"id":45,"title": "挑战您说我像谁我就模仿谁！第五期！这期更是很难绷！", "url": "https://www.bilibili.com/video/av116917952058927/", "flow": 2042000},
-{"id":46,"title": "今天来广东韶关，挑战在200多年历史的悬崖小屋上住一晚，体验一把野外生存", "url": "https://www.bilibili.com/video/av116911157288714/", "flow": 2395000},
-{"id":47,"title": "假如格林穿越到了re0的世界", "url": "https://www.bilibili.com/video/av116919394899984/", "flow": 656000},
-{"id":48,"title": "“我不在的日子 你身边的野花野草很嚣张啊！”", "url": "https://www.bilibili.com/video/av116911912257046/", "flow": 1228000},
-{"id":49,"title": "社交恐惧怎么办？", "url": "https://www.bilibili.com/video/av116906828826098/", "flow": 1263000},
-{"id":50,"title": "⚡️来个耄耋叫一叫⚡️～", "url": "https://www.bilibili.com/video/av116923706705682/", "flow": 1575000},
-{"id":51,"title": "600台冰箱要怎么用掉呢？", "url": "https://www.bilibili.com/video/av116916995752811/", "flow": 827000},
-{"id":52,"title": "ChiliChill乐团×花玲宴宁，《让风告诉你》提瓦特的故事吧！【BML-PLAY! 2026】", "url": "https://www.bilibili.com/video/av116873945417373/", "flow": 1903000},
-{"id":53,"title": "这期不笑的是给", "url": "https://www.bilibili.com/video/av116907013311126/", "flow": 1969000},
-{"id":54,"title": "真功夫的男人", "url": "https://www.bilibili.com/video/av116882971564012/", "flow": 1650000},
-{"id":55,"title": "属于我们的开拓之旅", "url": "https://www.bilibili.com/video/av116921592843644/", "flow": 747000},
-{"id":56,"title": "软烂脱骨嫩到离谱的美式烤肉", "url": "https://www.bilibili.com/video/av116921542382650/", "flow": 1148000},
-{"id":57,"title": "精致女生赛道整出狠活！水下露营让人肃然起敬堪比行为艺术哈哈哈", "url": "https://www.bilibili.com/video/av116916760872954/", "flow": 2398000},
-{"id":58,"title": "花4块钱买了一个遐蝶手办", "url": "https://www.bilibili.com/video/av116901007132004/", "flow": 1848000},
-{"id":59,"title": "这都能还原！！？【MC姬子篇】", "url": "https://www.bilibili.com/video/av116921475400544/", "flow": 1041000},
-{"id":60,"title": "破冰挑战！3小时摆摊PK，赚得多的获胜！！", "url": "https://www.bilibili.com/video/av116923522089906/", "flow": 2095000},
-{"id":61,"title": "《迈亚融解》实机首曝", "url": "https://www.bilibili.com/video/av116907214964195/", "flow": 4950000},
-{"id":62,"title": "今天不讲教学 想跟兄弟们聊聊", "url": "https://www.bilibili.com/video/av116923337673244/", "flow": 593000},
-{"id":63,"title": "质检遇到刺头？", "url": "https://www.bilibili.com/video/av116917700463323/", "flow": 1529000},
-{"id":64,"title": "沙耶之歌", "url": "https://www.bilibili.com/video/av116877586074957/", "flow": 1516000},
-{"id":65,"title": "人有五名，粉毛有三，流萤你不是其中之一", "url": "https://www.bilibili.com/video/av116923102661970/", "flow": 956000},
-{"id":66,"title": "动态视频｜世界上所有的钱，加起来有多少？", "url": "https://www.bilibili.com/video/av116912046545602/", "flow": 3391000},
-{"id":67,"title": "万万没想到", "url": "https://www.bilibili.com/video/av116916375128973/", "flow": 2470000},
-{"id":68,"title": "维多利亚时期老片-《牛排三明治》", "url": "https://www.bilibili.com/video/av116877804181057/", "flow": 678000},
-{"id":69,"title": "《办席竞赛》", "url": "https://www.bilibili.com/video/av116912382221224/", "flow": 3024000},
-{"id":70,"title": "【狂怒】乌鲁鲁首款干员研究外观7月17日上线！", "url": "https://www.bilibili.com/video/av116916442240312/", "flow": 3045000},
-{"id":71,"title": "《阴阳师》不相狐禅CG丨洪狐天运（CV：立花慎之介）", "url": "https://www.bilibili.com/video/av116928538547990/", "flow": 956000},
-{"id":72,"title": "父母没教的那些人情世故，你知道吗送礼领导家人礼物如何选择#人情世故#涨知识#年轻人#内容过于真实#送礼技巧", "url": "https://www.bilibili.com/video/av116912012987849/", "flow": 2744000},
-{"id":73,"title": "抓 住 了 重 点", "url": "https://www.bilibili.com/video/av116915972479735/", "flow": 1844000},
-{"id":74,"title": "「お疲れ。」", "url": "https://www.bilibili.com/video/av116903926369164/", "flow": 1890000},
-{"id":75,"title": "来了来了", "url": "https://www.bilibili.com/video/av116917884951524/", "flow": 1054000},
-{"id":76,"title": "《梦战：剑之海》概念先导CG首曝丨一个新的兰古利萨时代，即将开启", "url": "https://www.bilibili.com/video/av116910771477863/", "flow": 1313000},
-{"id":77,"title": "侠隐柱间&水门PV丨英雄见英雄，侠路再相逢【内含侠隐水门爆料】", "url": "https://www.bilibili.com/video/av116924126074409/", "flow": 1511000},
-{"id":78,"title": "木叶村男团神临BW", "url": "https://www.bilibili.com/video/av116913489316564/", "flow": 2286000},
-{"id":79,"title": "【群星】人类联邦第一个百年", "url": "https://www.bilibili.com/video/av116913254504381/", "flow": 2342000},
-{"id":80,"title": "尼古喵喵中最善良的角色房东，年轻时居然是帅哥？", "url": "https://www.bilibili.com/video/av116912113654751/", "flow": 1432000},
-{"id":81,"title": "《好好相处》", "url": "https://www.bilibili.com/video/av116917784352186/", "flow": 2054000},
-{"id":82,"title": "中大奖了！用药失效 病情复发了！剩一个药能用，祝我好运吧！", "url": "https://www.bilibili.com/video/av116912314914179/", "flow": 903000},
-{"id":83,"title": "《洛克王国：世界》远哥来了，福利稳了！", "url": "https://www.bilibili.com/video/av116924159626231/", "flow": 2464000},
-{"id":84,"title": "新三国up锐评神探狄仁杰06：你叫什么名字，做何营生，到幽州何干？", "url": "https://www.bilibili.com/video/av116919646555479/", "flow": 724000},
-{"id":85,"title": "都免疫“/kill”了非说是隐身，天堂装备没轻没重的【更好的MC】", "url": "https://www.bilibili.com/video/av116918287670203/", "flow": 1274000},
-{"id":86,"title": "这可能是最后一个正能量视频了，燃尽了兄弟们", "url": "https://www.bilibili.com/video/av116918774205700/", "flow": 557000},
-{"id":87,"title": "上海日式拉面挑战，超油腻！20分钟吃完奖金500元！能吃饱吗？", "url": "https://www.bilibili.com/video/av116917431960437/", "flow": 2183000},
-{"id":88,"title": "我的父亲 杀了我的父亲【完整版】", "url": "https://www.bilibili.com/video/av116912264713870/", "flow": 1957000},
-{"id":89,"title": "入驻B站一周年啦！", "url": "https://www.bilibili.com/video/av116917666909089/", "flow": 1080000},
-{"id":90,"title": "当二次元摄影第一次尝试拍星空…", "url": "https://www.bilibili.com/video/av116923102790560/", "flow": 964000},
-{"id":91,"title": "百万英镑，但是过于嚣张", "url": "https://www.bilibili.com/video/av116923035552563/", "flow": 735000},
-{"id":92,"title": "《明日方舟：终末地》干员叙事 [诀：履旧尘]", "url": "https://www.bilibili.com/video/av116918589588429/", "flow": 1426000},
-{"id":93,"title": "币战科研项目：姬七猫!6倍数值的隐藏装备？姬子启行的独特羁绊和超模数值重振反震荣光！", "url": "https://www.bilibili.com/video/av116921743708419/", "flow": 872000},
-{"id":94,"title": "【MCYT/FroggyDude/官方中文字幕】这个恐怖模组还以为能干掉我【微恐】", "url": "https://www.bilibili.com/video/av116907667688254/", "flow": 1012000},
-{"id":95,"title": "陈翔六点半：突然大方的兄弟", "url": "https://www.bilibili.com/video/av116917885015904/", "flow": 2394000},
-{"id":96,"title": "【货币战争攻略】试用姬子竟能吊打所有主C和生存位？全网最详细姬子A850稳定通关教程！货币战争零和博弈的最终答案 你说的对这就是四装备大人", "url": "https://www.bilibili.com/video/av116921760548651/", "flow": 1005000},
-{"id":97,"title": "宗主好舞不挑曲不是闹着玩的", "url": "https://www.bilibili.com/video/av116912767896996/", "flow": 805000},
-{"id":98,"title": "1100个小时，我的非洲之心终于回应了大家的祝福，也祝大家早日找到属于自己的非洲之心。", "url": "https://www.bilibili.com/video/av116916727386575/", "flow": 906000},
-{"id":99,"title": "有一个勇者前来斩杀魔王", "url": "https://www.bilibili.com/video/av116911895483577/", "flow": 1286000}
-]
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello Worlda!,my name is bird</h1>')
+  response.send('<h1>its my version 0.1 i am link by mysql</h1>')
 })
 
-app.get('/api/news', (request, response) => {
-  response.json(news)
+//app.get from mysql
+app.get('/api/news', async (request, response) => {
+    const [rows] = await pool.query('SELECT * FROM news')
+    response.json(rows)
 })
 
-app.get('/api/news/:id',(request,response) =>{
+//app.get from mysql by id
+app.get('/api/news/:id', async (request, response) => {
   const id = Number(request.params.id)
-  const newn = news.find(newn => newn.id === id)
-  if (newn) {
-    response.json(newn)
+  const [rows] = await pool.query('SELECT * FROM news WHERE id = ?', [id])
+  if (rows.length > 0) {
+    response.json(rows[0])
   } else {
     response.status(404).json({ error: 'news not found' })
   }
 })
+
 const PORT = process.env.PORT||3001
 app.listen(PORT, () => {
-  console.log('Server running on port ${PORT}')
+  console.log(`Server running on port ${PORT}`)
 })
 
